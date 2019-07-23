ca7d29eb20947ca2b370c3309bf3e51a6e3231f0
luongtx <tranxuanluong1998@gmail.com>
2019-07-23 20:09:31 +0700

    luong

--- js/home-page.js
+++ js/home-page.js
@@ -14,3 +14,19 @@ $(function() {
 		$(".slide2").toggleClass('phai');
 	});
 });
+
+$(window).scroll(function(){
+	var cur_width = parseInt($("body").css("width"));
+	if($(document).scrollTop()>50){
+		$("#brandimg").addClass("scrolledbrand");
+		$(".navbar-nav").addClass("scrollednav");
+		if(cur_width > 767) $(".collapse").css({"height":30});
+		else $(".collapse").css({"height":350});
+	}
+	else{
+		$("#brandimg").removeClass("scrolledbrand");
+		$(".navbar-nav").removeClass("scrollednav");
+		if(cur_width > 767) $(".collapse").css({"height":120});
+		else $(".collapse").css({"height":350});
+	}
+});
